5df6f68db08de2a6f983dbc9e5743ef50e972756
Luke Andrews <luke@attaboy.ca>
2019-10-22 18:02:57 -0400

    Remove colours from party definitions

--- pages/index.tsx
+++ pages/index.tsx
@@ -183,7 +183,7 @@ class Home extends React.Component<{}, State> {
   renderPartyInfo(party: Party, content?: React.ReactNode) {
     return (
       <span>
-        <span style={{ display: "inline-block", width: "0.9rem", height: "0.9rem", verticalAlign: "middle", backgroundColor: party.color, marginRight: "0.4em", marginBottom: "0.1rem", border: "1px solid black" }}></span>
+        <span className={party.className} style={{ display: "inline-block", width: "1rem", height: "1rem", verticalAlign: "top", marginTop: "0.15rem", marginRight: "0.4em", border: "1px solid black" }}></span>
         <span>{party[this.state.lang]}</span>
       </span>
     )
